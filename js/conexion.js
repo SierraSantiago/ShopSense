6b644f37e97ec45dda169d02083b516b02e6910b
SierraSantiago <SantiagoSierra003@gmail.com>
2023-10-17 10:04:05 -0500

    Conexion con database

---
+++ js/conexion.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'registroconsentimiento'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected to database with ID ' + connection.threadId);
+});
+
+module.exports = connection;
+
