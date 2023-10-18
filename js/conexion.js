c8df1deda663c7a95e83aa3310d8582f0d9d3c3b
SierraSantiago <SantiagoSierra003@gmail.com>
2023-10-17 20:55:15 -0500

    Error en extraer los datos y en funcion para aceptar habeas

--- js/conexion.js
+++ js/conexion.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const port = 3000;
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -9,12 +12,40 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) {
-        console.error('Error connecting to database: ' + err.stack);
-        return;
+        throw err;
     }
-
-    console.log('Connected to database with ID ' + connection.threadId);
+    console.log('Conexión exitosa a la base de datos');
 });
 
+//Mostrar
+connection.query('SELECT * FROM registroconsentimiento', (err, rows) => {
+    if (err) throw err;
+
+    console.log('Data received from Db:');
+    console.log(rows);
+});
+
+// Definir una ruta para obtener los datos
+app.get('/obtener-datos', (req, res) => {
+    // Realiza la consulta a la base de datos para obtener los datos
+    // Debes utilizar la lógica adecuada para tu base de datos y la consulta
+    const data = {
+        IdUsuario: 1001369917,
+        HabeasData: 0,
+        DateHabeasData: '2023-10-17 14:30:00',
+        Consentimientos: 0,
+        DateConsentimientos: '2023-10-17 14:30:00'
+    };
+    
+    // Envia los datos como respuesta en formato JSON
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor en ejecución en el puerto ${port}`);
+});
+
+
 module.exports = connection;
+
 
--- js/habeasData.js
+++ js/habeasData.js
@@ -1,29 +1,33 @@
+const connection = require('../js/conexion'); // Asegúrate de ajustar la ruta al archivo db.js si es necesario
 
-        // Obtener elementos del DOM
-        const popup = document.getElementById('popup');
-        const closePopup = document.getElementById('close-popup');
-        const acceptButton = document.getElementById('accept');
-        const denyButton = document.getElementById('denegar');
-        const withdrawLink = document.getElementById('withdraw');
+// Obtener elementos del DOM
+const popup = document.getElementById('popup');
+const closePopup = document.getElementById('close-popup');
+const acceptButton = document.getElementById('accept');
+const denyButton = document.getElementById('denegar');
+const withdrawLink = document.getElementById('withdraw');
 
-        // Mostrar el pop-up al cargar la página
-        window.onload = function() {
-            popup.style.display = 'block';
-        };
 
-        // Cerrar el pop-up al hacer clic en la X
-        closePopup.onclick = function() {
-            popup.style.display = 'none';
-        };
+// Mostrar el pop-up al cargar la página
+window.onload = function () {
+    popup.style.display = 'block';
+};
 
-        // Cerrar el pop-up al hacer clic en "Aceptar"
-        acceptButton.onclick = function() {
-            popup.style.display = 'none';
-        };
+// Cerrar el pop-up al hacer clic en la X
+closePopup.onclick = function () {
+    popup.style.display = 'none';
+};
 
-        // Acción al hacer clic en "Retirar Consentimiento"
-        denyButton.onclick = function() {
-            popup.style.display = 'none';
-            // Aquí puedes implementar la lógica para retirar el consentimiento
-            alert('Consentimiento retirado. Sus datos no serán recopilados.');
-        };
+// Cerrar el pop-up al hacer clic en "Aceptar"
+acceptButton.onclick = function () {
+    popup.style.display = 'none';
+    connection.query('UPDATE registroconsentimiento SET HabeasData = 1,DateHabeasData = NOW()  WHERE IdUsuario = 1001369917');
+};
+
+// Acción al hacer clic en "Retirar Consentimiento"
+denyButton.onclick = function () {
+    popup.style.display = 'none';
+    // Aquí puedes implementar la lógica para retirar el consentimiento
+    alert('Consentimiento retirado. Sus datos no serán recopilados.');
+};
+
